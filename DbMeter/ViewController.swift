ff94f3e46409003e558997fd91f56a3838ee896e
Kevin Kuan <kevinkuan0.app@outlook.com>
2023-02-17 18:05:48 -0800

    Initial Commit

---
+++ DbMeter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DbMeter
+//
+//  Created by Kevin Kuan on 2023/2/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
