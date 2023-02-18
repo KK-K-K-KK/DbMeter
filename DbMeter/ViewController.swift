151258f41055c226d026bbd0c1807f6f4439ad11
Kevin Kuan <kevinkuan0.app@outlook.com>
2023-02-17 19:20:21 -0800

    Microphone setup - Ask for permission on viewDidLoad - Open AV recording session

--- DbMeter/ViewController.swift
+++ DbMeter/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setUpAudioCapture()
     }
 
-
+    private func setUpAudioCapture() {
+        let recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord)
+            try recordingSession.setActive(true)
+            
+            recordingSession.requestRecordPermission({
+                result in
+                guard result else {return}
+            })
+        } catch {
+            print("ERROR: Failed to set up recording session. Have you enabled the recording permission?")
+        }
+    }
 }
 
